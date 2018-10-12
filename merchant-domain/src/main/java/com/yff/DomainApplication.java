cb0c0e5d46f968e4826c35bf423d41c9e6ebf081
xyzmonday_glb <upcyff_glb@126.com>
2018-10-12 18:42:01 +0800

    第一次提交

---
+++ merchant-domain/src/main/java/com/yff/DomainApplication.java
@@ -0,0 +1,24 @@
+package com.yff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+public class DomainApplication {
+    /**
+     * 访问地址 http://localhost:8080/actuator/hystrix.stream看响应数据
+     * http://localhost:8080/hystrix打开监控仪表盘
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(DomainApplication.class,args);
+    }
+}
